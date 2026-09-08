76af36993524dcb4d192a6e1c7e838a8016f745a
Sejal Kumari <sejalkumari917@gmail.com>
2026-09-08 15:45:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (45.51%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>=10)
+        {
+            int sum=0;
+            while(num>0)
+            {
+                int lastdigit=num%10;
+                sum=sum+lastdigit;
+                num=num/10;
+            }
+             num=sum;
+        }
+            return num; 
+    }
+};
